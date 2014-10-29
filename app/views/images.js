f136082a0de09f1429f894a0d80a490f5bf8d2e2
Matti Salokangas <matti.salokangas@gmail.com>
2014-10-29 15:01:36 -0700

    Remove unused self reference

--- app/views/images.js
+++ app/views/images.js
@@ -2,11 +2,10 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   didInsertElement: function() {
-      var self = this;
       Ember.run.scheduleOnce('afterRender', function () {
         Ember.run.later(function() {
-          new CBPGridGallery( document.getElementById( 'grid-gallery' ) )
+          new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
         }, 2000);
-      })
+      });
   }
 });
