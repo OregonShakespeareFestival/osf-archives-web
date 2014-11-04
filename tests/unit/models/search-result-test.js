9a103399786e2774ea4f23bfb058544df43bda2e
Matti Salokangas <matti.salokangas@gmail.com>
2014-11-04 15:23:09 -0800

    Add unit test model for search result

--- tests/unit/models/search-result-test.js
+++ tests/unit/models/search-result-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForModel('search-result-item', 'SearchResultItem', {
+moduleForModel('search-result', 'SearchResult', {
   // Specify the other units that are required for this test.
   needs: []
 });
